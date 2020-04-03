78b9a69be977a1ddc6c55a104af757cecc4c2a3c
bugagashenkj <bugagashenkj@gmail.com>
2020-04-03 19:07:37 +0300

    Implement first task

--- build/cmd/bood/main.go
+++ build/cmd/bood/main.go
@@ -4,9 +4,7 @@ import (
 	"flag"
 	"github.com/google/blueprint"
 	"github.com/roman-mazur/bood"
-	"github.com/roman-mazur/bood/gomodule"
-	// TODO: Підставте свій власний пакет.
-	// "github.com/roman-mazur/design-practice-2-template/build/gomodule"
+	"github.com/KPI-Labs/design-lab-2/build/gomodule"
 	"io/ioutil"
 	"log"
 	"os"
@@ -20,7 +18,6 @@ var (
 
 func NewContext() *blueprint.Context {
 	ctx := bood.PrepareContext()
-	// TODO: Замініть імплементацію go_binary на власну.
 	ctx.RegisterModuleType("go_binary", gomodule.SimpleBinFactory)
 	return ctx
 }
